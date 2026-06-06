3c260d67b4366748f89cb6e34c5ecd31c0ff634f
ruhaandullu11-maker <ruhaandullu11@gmail.com>
2026-06-06 12:41:53 +0000

    cv1 of bc.py

---
+++ BC.py
@@ -0,0 +1,70 @@
+import datetime as dt
+import time
+
+print('Welcome to the bday countdown')
+
+
+year = int(input('What year were you born in?\n'))
+month = int(input('What month (1 for January, 2 for Febuary,etc...)?\n'))
+day = int(input('What day in that month?\n'))
+
+dateofbirth = dt.datetime(year, month, day)
+
+weekdaynames = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+weekdaynum = dateofbirth.weekday()
+print('I am pretty sure have forgotten what day of the week it was ...')
+print('It was a...let me think...', end = ' ')
+print('Oh yeah it was a ',weekdaynames[weekdaynum])
+
+timern = dt.datetime.now()
+thisyear = timern.year
+thisbday = dt.datetime(thisyear, month, day)
+
+
+
+if thisbday > timern:
+    nextbday = thisbday
+else:
+    nextbday = dt.datetime(thisyear+1, month, day)
+
+print('Your next bday will be on the...', end = ' ')
+print(nextbday)
+print()
+print('Which will be a...', end = ' ')
+weekdaynum = nextbday.weekday()
+print(weekdaynames[weekdaynum])
+print()
+
+d = nextbday - timern
+
+totsecleft = d.seconds
+dysleft = d.days
+
+secondsleft = totsecleft % 60
+totminsleft = totsecleft //60
+hoursleft = totminsleft //60
+minsleft = totminsleft % 60
+
+
+totminsleft, secondsleft  = divmod(totsecleft, 60)
+hoursleft, minsleft  = divmod(totminsleft, 60)
+
+
+
+
+while nextbday > timern:
+    
+    timern = dt.datetime.now()
+    d = nextbday - timern
+    dysleft = d.days
+    totsecleft = d.seconds
+
+
+    secondsleft = totsecleft % 60
+    totminsleft = totsecleft //60
+    hoursleft = totminsleft //60
+    minsleft = totminsleft % 60
+
+    print('Your next birthday will be in', dysleft, 'days', hoursleft, 'hrs', minsleft, 'mins', secondsleft, 'secs away.')
+
+    time.sleep(1)
